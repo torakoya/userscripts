42d1d54ce9d54b8e0390d150b9b00e61c42f5b35
Yoichiro Takahashi <y@ps.st>
2021-03-12 15:25:19 +0900

    Gyao More Button Reviver: automatically done

--- gyao_more_button_reviver.user.js
+++ gyao_more_button_reviver.user.js
@@ -2,7 +2,7 @@
 // @name         Gyao More Button Reviver
 // @namespace    http://y.ps.st/
 // @version      0.1
-// @description  Revive the More buttons that disappear on error in the new arrivals pages on Gyao by pressing Ctrl+Meta+M
+// @description  Revive the More buttons that disappear on error in the new arrivals pages on Gyao
 // @author       Yoichiro Takahashi
 // @match        *://gyao.yahoo.co.jp/arrivals
 // @match        *://gyao.yahoo.co.jp/arrivals/*
@@ -12,13 +12,19 @@
 (function () {
     "use strict";
 
-    document.addEventListener("keydown", event => {
-        if (event.code == "KeyM" && event.ctrlKey && event.metaKey) {
-            let ee = document.querySelectorAll(".video-list-lazy.is-ended");
-            for (let e of ee) {
+    let o = new MutationObserver(mutations => {
+        mutations.forEach(m => {
+            let e = m.target;
+            if (e.classList.contains("is-ended") && !e.classList.contains("is-loading")) {
                 e.classList.remove("is-ended");
                 e.lazyLoader.state.requestTimes--;
+                alert("The More button has been shown again by force. It was hidden, which means a server error occurred or you reached the end of the list.");
             }
-        }
+        });
     });
+
+    let ee = document.querySelectorAll(".video-list-lazy");
+    for (let e of ee) {
+        o.observe(e, {attributes: true});
+    }
 })();
